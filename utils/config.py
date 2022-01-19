40404f91529dac19f352f7c19fd032dc4896542d
saswat0 <saswatsubhajyotimallick@gmail.com>
2022-01-19 15:33:08 +0530

    added inital files

---
+++ utils/config.py
@@ -0,0 +1,47 @@
+cfg = {
+    # general settings
+    "batch_size": 32,
+    "input_size": (240, 320),
+
+    # training dataset
+    "dataset_path": 'dataset/train_mask.tfrecord',
+    "val_path": 'dataset/val_mask.tfrecord',
+    "dataset_len": 6115,
+    "val_len": 1839,
+    "using_crop": True,
+    "using_bin": True,
+    "using_flip": True,
+    "using_distort": True,
+    "using_normalizing": True,
+    "labels_list": ['background', 'mask', 'unmask'],
+
+    # anchor settings
+    # "min_sizes": [[(9, 7), (24, 20), (39, 35)], [(54, 41), (65, 61), (81, 66)],
+    #               [(94, 86), (113, 95), (131, 122)], [(137, 128), (172, 162), (176, 210)]],
+    "min_sizes":[[10, 16, 24], [32, 48], [64, 96], [128, 192, 256]],
+    "steps": [8, 16, 32, 64],
+    "match_thresh": 0.45,
+    "variances": [0.1, 0.2],
+    "clip": False,
+
+    # network
+    "base_channel": 16,
+
+    # training settings
+    "resume": False,
+    "epoch": 100,
+    "init_lr": 1e-2,
+    "lr_decay_epoch": [50, 70],
+    "lr_rate": 0.1,
+    "warmup_epoch": 5,
+    "min_lr": 1e-4,
+
+    "weights_decay": 5e-4,
+    "momentum": 0.9,
+    "save_freq": 1,
+
+    # inference
+    "score_threshold": 0.5,
+    "nms_threshold": 0.4,
+    "max_number_keep": 200
+}
---
+++ utils/prior_box.py
@@ -0,0 +1,57 @@
+import math,sys
+import numpy as np
+from itertools import product as product
+
+def priors_box(cfg,image_sizes=None):
+    if image_sizes is None:
+        image_sizes = cfg['input_size']
+    min_sizes=cfg["min_sizes"]
+    steps=cfg["steps"]
+    clip=cfg["clip"]
+
+    if isinstance(image_sizes, int):
+        image_sizes = (image_sizes, image_sizes)
+    elif isinstance(image_sizes, tuple):
+        image_sizes = image_sizes
+    else:
+        raise Exception('Type error of input image size format,tuple or int. ')
+
+    for m in range(4):
+        if (steps[m] != pow(2, (m + 3))):
+            print("steps must be [8,16,32,64]")
+            sys.exit()
+
+    assert len(min_sizes) == len(steps), "anchors number didn't match the feature map layer."
+
+    feature_maps = [
+        [math.ceil(image_sizes[0] / step), math.ceil(image_sizes[1] / step)]
+        for step in steps]
+
+    anchors = []
+    num_box_fm_cell=[]
+    for k, f in enumerate(feature_maps):
+        num_box_fm_cell.append(len(min_sizes[k]))
+        for i, j in product(range(f[0]), range(f[1])):
+            for min_size in min_sizes[k]:
+                if isinstance(min_size, int):
+                    min_size = (min_size, min_size)
+                elif isinstance(min_size, tuple):
+                    min_size=min_size
+                else:
+                    raise Exception('Type error of min_sizes elements format,tuple or int. ')
+                s_kx = min_size[1] / image_sizes[1]
+                s_ky = min_size[0] / image_sizes[0]
+                cx = (j + 0.5) * steps[k] / image_sizes[1]
+                cy = (i + 0.5) * steps[k] / image_sizes[0]
+                anchors += [cx, cy, s_kx, s_ky]
+
+    output = np.asarray(anchors).reshape([-1, 4])
+    if clip:
+        output = np.clip(output, 0, 1)
+    return output,num_box_fm_cell
+
+
+
+if __name__ == '__main__':
+    import config
+    priors_box(config.cfg)
