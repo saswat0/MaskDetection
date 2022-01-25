50a9965d94d5cca64ccaa3576da78f6e6aafb197
saswat0 <saswatsubhajyotimallick@gmail.com>
2022-01-25 10:51:33 +0530

    added utilities

---
+++ utils.py
@@ -0,0 +1,455 @@
+import json
+import os
+import torch
+import random
+import xml.etree.ElementTree as ET
+import torchvision.transforms.functional as FT
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+voc_labels = ('nospeedsign', 'speedlimit sign')
+label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
+label_map['background'] = 0
+rev_label_map = {v: k for k, v in label_map.items()}
+
+distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+                   '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000',
+                   '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+
+
+def parse_annotation(annotation_path):
+    tree = ET.parse(annotation_path)
+    root = tree.getroot()
+
+    boxes = list()
+    labels = list()
+    difficulties = list()
+    for object in root.iter('object'):
+
+        difficult = int(object.find('difficult').text == '1')
+
+        label = object.find('name').text.lower().strip()
+        if label not in label_map:
+            continue
+
+        bbox = object.find('bndbox')
+        xmin = int(bbox.find('xmin').text) - 1
+        ymin = int(bbox.find('ymin').text) - 1
+        xmax = int(bbox.find('xmax').text) - 1
+        ymax = int(bbox.find('ymax').text) - 1
+
+        boxes.append([xmin, ymin, xmax, ymax])
+        labels.append(label_map[label])
+        difficulties.append(difficult)
+
+    return {'boxes': boxes, 'labels': labels, 'difficulties': difficulties}
+
+
+def create_data_lists(voc07_path, voc12_path, output_folder):
+    voc07_path = os.path.abspath(voc07_path)
+    voc12_path = os.path.abspath(voc12_path)
+
+    train_images = list()
+    train_objects = list()
+    n_objects = 0
+
+    for path in [voc07_path]:
+
+        with open(os.path.join(path, 'ImageSets/Main/trainval.txt')) as f:
+            ids = f.read().splitlines()
+
+        for id in ids:
+            objects = parse_annotation(os.path.join(path, 'Annotations', id + '.xml'))
+            if len(objects['boxes']) == 0:
+                continue
+            n_objects += len(objects)
+            train_objects.append(objects)
+            train_images.append(os.path.join(path, 'JPEGImages', id + '.jpg'))
+
+    assert len(train_objects) == len(train_images)
+
+    with open(os.path.join(output_folder, 'TRAIN_images.json'), 'w') as j:
+        json.dump(train_images, j)
+    with open(os.path.join(output_folder, 'TRAIN_objects.json'), 'w') as j:
+        json.dump(train_objects, j)
+    with open(os.path.join(output_folder, 'label_map.json'), 'w') as j:
+        json.dump(label_map, j)
+
+    print('\nThere are %d training images containing a total of %d objects. Files have been saved to %s.' % (
+        len(train_images), n_objects, os.path.abspath(output_folder)))
+
+    test_images = list()
+    test_objects = list()
+    n_objects = 0
+
+    with open(os.path.join(voc07_path, 'ImageSets/Main/test.txt')) as f:
+        ids = f.read().splitlines()
+
+    for id in ids:
+        objects = parse_annotation(os.path.join(voc07_path, 'Annotations', id + '.xml'))
+        if len(objects) == 0:
+            continue
+        test_objects.append(objects)
+        n_objects += len(objects)
+        test_images.append(os.path.join(voc07_path, 'JPEGImages', id + '.jpg'))
+
+    assert len(test_objects) == len(test_images)
+
+    with open(os.path.join(output_folder, 'TEST_images.json'), 'w') as j:
+        json.dump(test_images, j)
+    with open(os.path.join(output_folder, 'TEST_objects.json'), 'w') as j:
+        json.dump(test_objects, j)
+
+    print('\nThere are %d test images containing a total of %d objects. Files have been saved to %s.' % (
+        len(test_images), n_objects, os.path.abspath(output_folder)))
+
+
+def decimate(tensor, m):
+    assert tensor.dim() == len(m)
+    for d in range(tensor.dim()):
+        if m[d] is not None:
+            tensor = tensor.index_select(dim=d,
+                                         index=torch.arange(start=0, end=tensor.size(d), step=m[d]).long())
+
+    return tensor
+
+
+def calculate_mAP(det_boxes, det_labels, det_scores, true_boxes, true_labels, true_difficulties):
+    assert len(det_boxes) == len(det_labels) == len(det_scores) == len(true_boxes) == len(
+        true_labels) == len(
+        true_difficulties)
+    n_classes = len(label_map)
+
+    true_images = list()
+    for i in range(len(true_labels)):
+        true_images.extend([i] * true_labels[i].size(0))
+
+    true_images = torch.LongTensor(true_images).to(device)
+    true_boxes = torch.cat(true_boxes, dim=0)
+    true_labels = torch.cat(true_labels, dim=0)
+    true_difficulties = torch.cat(true_difficulties, dim=0)
+
+    assert true_images.size(0) == true_boxes.size(0) == true_labels.size(0)
+
+    det_images = list()
+    for i in range(len(det_labels)):
+        det_images.extend([i] * det_labels[i].size(0))
+    det_images = torch.LongTensor(det_images).to(device)
+    det_boxes = torch.cat(det_boxes, dim=0)
+    det_labels = torch.cat(det_labels, dim=0)
+    det_scores = torch.cat(det_scores, dim=0)
+
+    assert det_images.size(0) == det_boxes.size(0) == det_labels.size(0) == det_scores.size(0)
+
+    average_precisions = torch.zeros((n_classes - 1), dtype=torch.float)
+    for c in range(1, n_classes):
+        true_class_images = true_images[true_labels == c]
+        true_class_boxes = true_boxes[true_labels == c]
+        true_class_difficulties = true_difficulties[true_labels == c]
+        n_easy_class_objects = (1 - true_class_difficulties).sum().item()
+
+        true_class_boxes_detected = torch.zeros((true_class_difficulties.size(0)), dtype=torch.uint8).to(device)
+
+        det_class_images = det_images[det_labels == c]
+        det_class_boxes = det_boxes[det_labels == c]
+        det_class_scores = det_scores[det_labels == c]
+        n_class_detections = det_class_boxes.size(0)
+        if n_class_detections == 0:
+            continue
+
+        det_class_scores, sort_ind = torch.sort(det_class_scores, dim=0, descending=True)
+        det_class_images = det_class_images[sort_ind]
+        det_class_boxes = det_class_boxes[sort_ind]
+
+        true_positives = torch.zeros((n_class_detections), dtype=torch.float).to(device)
+        false_positives = torch.zeros((n_class_detections), dtype=torch.float).to(device)
+        for d in range(n_class_detections):
+            this_detection_box = det_class_boxes[d].unsqueeze(0)
+            this_image = det_class_images[d]
+
+            object_boxes = true_class_boxes[true_class_images == this_image]
+            object_difficulties = true_class_difficulties[true_class_images == this_image]
+
+            if object_boxes.size(0) == 0:
+                false_positives[d] = 1
+                continue
+
+            overlaps = find_jaccard_overlap(this_detection_box, object_boxes)
+            max_overlap, ind = torch.max(overlaps.squeeze(0), dim=0)
+
+            original_ind = torch.LongTensor(range(true_class_boxes.size(0)))[true_class_images == this_image][ind]
+
+            if max_overlap.item() > 0.5:
+                if object_difficulties[ind] == 0:
+                    if true_class_boxes_detected[original_ind] == 0:
+                        true_positives[d] = 1
+                        true_class_boxes_detected[original_ind] = 1
+                    else:
+                        false_positives[d] = 1
+            else:
+                false_positives[d] = 1
+
+        cumul_true_positives = torch.cumsum(true_positives, dim=0)
+        cumul_false_positives = torch.cumsum(false_positives, dim=0)
+        cumul_precision = cumul_true_positives / (
+                cumul_true_positives + cumul_false_positives + 1e-10)
+        cumul_recall = cumul_true_positives / n_easy_class_objects
+
+        recall_thresholds = torch.arange(start=0, end=1.1, step=.1).tolist()
+        precisions = torch.zeros((len(recall_thresholds)), dtype=torch.float).to(device)
+        for i, t in enumerate(recall_thresholds):
+            recalls_above_t = cumul_recall >= t
+            if recalls_above_t.any():
+                precisions[i] = cumul_precision[recalls_above_t].max()
+            else:
+                precisions[i] = 0.
+        average_precisions[c - 1] = precisions.mean()
+
+    mean_average_precision = average_precisions.mean().item()
+
+    average_precisions = {rev_label_map[c + 1]: v for c, v in enumerate(average_precisions.tolist())}
+
+    return average_precisions, mean_average_precision
+
+
+def xy_to_cxcy(xy):
+    return torch.cat([(xy[:, 2:] + xy[:, :2]) / 2,
+                      xy[:, 2:] - xy[:, :2]], 1)
+
+
+def cxcy_to_xy(cxcy):
+    return torch.cat([cxcy[:, :2] - (cxcy[:, 2:] / 2),
+                      cxcy[:, :2] + (cxcy[:, 2:] / 2)], 1)
+
+
+def cxcy_to_gcxgcy(cxcy, priors_cxcy):
+    return torch.cat([(cxcy[:, :2] - priors_cxcy[:, :2]) / (priors_cxcy[:, 2:] / 10),
+                      torch.log(cxcy[:, 2:] / priors_cxcy[:, 2:]) * 5], 1)
+
+
+def gcxgcy_to_cxcy(gcxgcy, priors_cxcy):
+    return torch.cat([gcxgcy[:, :2] * priors_cxcy[:, 2:] / 10 + priors_cxcy[:, :2],
+                      torch.exp(gcxgcy[:, 2:] / 5) * priors_cxcy[:, 2:]], 1)
+
+
+def find_intersection(set_1, set_2):
+    lower_bounds = torch.max(set_1[:, :2].unsqueeze(1), set_2[:, :2].unsqueeze(0))
+    upper_bounds = torch.min(set_1[:, 2:].unsqueeze(1), set_2[:, 2:].unsqueeze(0))
+    intersection_dims = torch.clamp(upper_bounds - lower_bounds, min=0)
+    return intersection_dims[:, :, 0] * intersection_dims[:, :, 1]
+
+
+def find_jaccard_overlap(set_1, set_2):
+    
+    intersection = find_intersection(set_1, set_2)
+
+    areas_set_1 = (set_1[:, 2] - set_1[:, 0]) * (set_1[:, 3] - set_1[:, 1])
+    areas_set_2 = (set_2[:, 2] - set_2[:, 0]) * (set_2[:, 3] - set_2[:, 1])
+
+    union = areas_set_1.unsqueeze(1) + areas_set_2.unsqueeze(0) - intersection
+
+    return intersection / union
+
+
+def expand(image, boxes, filler):
+    original_h = image.size(1)
+    original_w = image.size(2)
+    max_scale = 4
+    scale = random.uniform(1, max_scale)
+    new_h = int(scale * original_h)
+    new_w = int(scale * original_w)
+
+    filler = torch.FloatTensor(filler)
+    new_image = torch.ones((3, new_h, new_w), dtype=torch.float) * filler.unsqueeze(1).unsqueeze(1)
+
+    left = random.randint(0, new_w - original_w)
+    right = left + original_w
+    top = random.randint(0, new_h - original_h)
+    bottom = top + original_h
+    new_image[:, top:bottom, left:right] = image
+
+    new_boxes = boxes + torch.FloatTensor([left, top, left, top]).unsqueeze(0)
+
+    return new_image, new_boxes
+
+
+def random_crop(image, boxes, labels, difficulties):
+    
+    original_h = image.size(1)
+    original_w = image.size(2)
+    
+    while True:
+        min_overlap = random.choice([0., .1, .3, .5, .7, .9, None])
+
+        if min_overlap is None:
+            return image, boxes, labels, difficulties
+
+        max_trials = 50
+        for _ in range(max_trials):
+            min_scale = 0.3
+            scale_h = random.uniform(min_scale, 1)
+            scale_w = random.uniform(min_scale, 1)
+            new_h = int(scale_h * original_h)
+            new_w = int(scale_w * original_w)
+
+            aspect_ratio = new_h / new_w
+            if not 0.5 < aspect_ratio < 2:
+                continue
+
+            left = random.randint(0, original_w - new_w)
+            right = left + new_w
+            top = random.randint(0, original_h - new_h)
+            bottom = top + new_h
+            crop = torch.FloatTensor([left, top, right, bottom])
+
+            overlap = find_jaccard_overlap(crop.unsqueeze(0), boxes)
+            overlap = overlap.squeeze(0)
+
+            if overlap.max().item() < min_overlap:
+                continue
+
+            new_image = image[:, top:bottom, left:right]
+
+            bb_centers = (boxes[:, :2] + boxes[:, 2:]) / 2. 
+
+            centers_in_crop = (bb_centers[:, 0] > left) * (bb_centers[:, 0] < right) * (bb_centers[:, 1] > top) * (
+                    bb_centers[:, 1] < bottom)
+
+            if not centers_in_crop.any():
+                continue
+
+            new_boxes = boxes[centers_in_crop, :]
+            new_labels = labels[centers_in_crop]
+            new_difficulties = difficulties[centers_in_crop]
+
+            new_boxes[:, :2] = torch.max(new_boxes[:, :2], crop[:2])
+            new_boxes[:, :2] -= crop[:2]
+            new_boxes[:, 2:] = torch.min(new_boxes[:, 2:], crop[2:])
+            new_boxes[:, 2:] -= crop[:2]
+
+            return new_image, new_boxes, new_labels, new_difficulties
+
+
+def flip(image, boxes):
+    new_image = FT.hflip(image)
+
+    new_boxes = boxes
+    new_boxes[:, 0] = image.width - boxes[:, 0] - 1
+    new_boxes[:, 2] = image.width - boxes[:, 2] - 1
+    new_boxes = new_boxes[:, [2, 1, 0, 3]]
+
+    return new_image, new_boxes
+
+
+def resize(image, boxes, dims=(300, 300), return_percent_coords=True):
+    new_image = FT.resize(image, dims)
+
+    old_dims = torch.FloatTensor([image.width, image.height, image.width, image.height]).unsqueeze(0)
+    new_boxes = boxes / old_dims
+
+    if not return_percent_coords:
+        new_dims = torch.FloatTensor([dims[1], dims[0], dims[1], dims[0]]).unsqueeze(0)
+        new_boxes = new_boxes * new_dims
+
+    return new_image, new_boxes
+
+
+def photometric_distort(image):
+    new_image = image
+
+    distortions = [FT.adjust_brightness,
+                   FT.adjust_contrast,
+                   FT.adjust_saturation,
+                   FT.adjust_hue]
+
+    random.shuffle(distortions)
+
+    for d in distortions:
+        if random.random() < 0.5:
+            if d.__name__ is 'adjust_hue':
+                adjust_factor = random.uniform(-18 / 255., 18 / 255.)
+            else:
+                adjust_factor = random.uniform(0.5, 1.5)
+
+            new_image = d(new_image, adjust_factor)
+
+    return new_image
+
+
+def transform(image, boxes, labels, difficulties, split):
+    assert split in {'TRAIN', 'TEST'}
+
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    new_image = image
+    new_boxes = boxes
+    new_labels = labels
+    new_difficulties = difficulties
+    if split == 'TRAIN':
+        new_image = photometric_distort(new_image)
+
+        new_image = FT.to_tensor(new_image)
+
+        if random.random() < 0.5:
+            new_image, new_boxes = expand(new_image, boxes, filler=mean)
+
+        new_image, new_boxes, new_labels, new_difficulties = random_crop(new_image, new_boxes, new_labels, new_difficulties)
+        new_image = FT.to_pil_image(new_image)
+
+        if random.random() < 0.5:
+            new_image, new_boxes = flip(new_image, new_boxes)
+
+    new_image, new_boxes = resize(new_image, new_boxes, dims=(300, 300))
+    new_image = FT.to_tensor(new_image)
+    new_image = FT.normalize(new_image, mean=mean, std=std)
+
+    return new_image, new_boxes, new_labels, new_difficulties
+
+
+def adjust_learning_rate(optimizer, scale):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * scale
+    print("DECAYING learning rate.\n The new LR is %f\n" % (optimizer.param_groups[1]['lr'],))
+
+
+def accuracy(scores, targets, k):
+    batch_size = targets.size(0)
+    _, ind = scores.topk(k, 1, True, True)
+    correct = ind.eq(targets.view(-1, 1).expand_as(ind))
+    correct_total = correct.view(-1).float().sum()
+    return correct_total.item() * (100.0 / batch_size)
+
+
+def save_checkpoint(epoch, model, optimizer):
+    state = {'epoch': epoch,
+             'model': model,
+             'optimizer': optimizer}
+    filename = 'checkpoint_ssd300.pth.tar'
+    torch.save(state, filename)
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def clip_gradient(optimizer, grad_clip):
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+
